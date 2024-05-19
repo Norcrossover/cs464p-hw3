49d8d6d427f6df930bdd9904389236cab4a73424
KobeNorcross <kobenorcross@gmail.com>
2024-05-18 21:42:36 -0700

    Styled home and navbar

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -2,13 +2,13 @@ import Root from "./root";
 
 export default function Home() {
   return (
-    <div className="flex-col bg-slate-900 text-slate-200 min-h-screen ">
+    <div className="flex-col bg-slate-900 text-slate-100 min-h-screen">
       <Root />
-      <h1 className="text-4xl font-bold p-5 text-center">
+      <h1 className="text-4xl font-bold p-5 m-5 text-center">
         Welcome to Homework 3 for Frontend Web Development!
       </h1>
-      <section className="flex m-5 p-5 bg-pink-700 rounded-md">
-        <h2 className="text-2xl font-semibold p-6 text-center break-words">
+      <section className="flex flex-col lg:flex-row m-5 p-5 bg-gradient-to-r from-pink-500 to-pink-900 rounded-md text-slate-100">
+        <h2 className="text-2xl font-semibold text-white p-6 text-center break-words">
           Interested in information on a specific country, find it in the List
           tab.
         </h2>
@@ -18,7 +18,7 @@ export default function Home() {
           className="h-[200px] justify-self-end"
         />
       </section>
-      <section className="flex m-5 p-5 bg-cyan-700 rounded-md">
+      <section className="flex flex-col lg:flex-row m-5 p-5 bg-gradient-to-r from-cyan-500 to-cyan-900 rounded-md">
         <img
           src="../public/PopulationPage.png"
           alt="An image showing the Population tab"
@@ -29,7 +29,7 @@ export default function Home() {
           Navigate to the population tab.
         </h2>
       </section>
-      <section className="flex m-5 p-5 bg-pink-700 rounded-md">
+      <section className="flex flex-col lg:flex-row m-5 p-5 bg-gradient-to-r from-slate-500 to-slate-900 rounded-md">
         <h2 className="text-2xl font-semibold p-6">
           Lastly, find a chart displaying information on the Languages spoken in
           these countries in the Languages tab.
